1e28c075c87b34f813f1b0a4c8a30cffffd27dde
akups <appiah-lindberg@outlook.com>
2020-02-28 00:42:01 +0100

    conflicts resolved

--- starter-code/frontend/src/components/Signup.js
+++ starter-code/frontend/src/components/Signup.js
@@ -11,7 +11,6 @@ class Signup extends Component {
     role: ""
   };
 
-
   handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
@@ -33,146 +32,140 @@ class Signup extends Component {
       .then(response => {
         this.props.history.push("/");
 
-<<<<<<< HEAD
-  //         this.props.setUser(response.data);
-  //       })
-  //       .catch(err => {
-  //         this.setState({
-  //           message: err.response.data.message
-  //           /* setTimeout(() => {
-  //           this.props.history.push("/");
-  //         }, 3000); */
-  //         });
-  //       });
-  //   };
-=======
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log("eventtarget", this.state);
+        //         this.props.setUser(response.data);
+        //       })
+        //       .catch(err => {
+        //         this.setState({
+        //           message: err.response.data.message
+        //           /* setTimeout(() => {
+        //           this.props.history.push("/");
+        //         }, 3000); */
+        //         });
+        //       });
+        //   };
+        handleSubmit = event => {
+          event.preventDefault();
+          console.log("eventtarget", this.state);
 
-    axios
-      .post("/api/auth/signup", {
-        name: this.state.name,
-        email: this.state.email,
-        password: this.state.password,
-        role: this.state.role
-      })
-      .then(response => {
-        console.log("RSPONSE", response);
+          axios
+            .post("/api/auth/signup", {
+              name: this.state.name,
+              email: this.state.email,
+              password: this.state.password,
+              role: this.state.role
+            })
+            .then(response => {
+              console.log("RSPONSE", response);
 
-        //this.props.history.push("/");
+              //this.props.history.push("/");
 
-        //this.props.setUser(this.response.data);
-=======
-        this.props.setUser(response.data);
->>>>>>> f9f9160c96d05acf545ae18714417ef66412d9d4
-      })
-      .catch(err => {
-        this.setState({
-          message: err.response.data.message
-<<<<<<< HEAD
-        });
-      });
-  };
+              //this.props.setUser(this.response.data);
+              this.props.setUser(response.data);
+            })
+            .catch(err => {
+              this.setState({
+                message: err.response.data.message
+              });
+            });
+        };
 
-  setFormState = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
+        setFormState = event => {
+          this.setState({
+            [event.target.name]: event.target.value
+          });
+        };
 
-  onChange = event => {
-    this.setState({ role: event.target.value });
-  };
->>>>>>> akua
-=======
-          /* setTimeout(() => {
+        onChange = event => {
+          this.setState({ role: event.target.value });
+        };
+
+        /* setTimeout(() => {
             this.props.history.push("/");
           }, 3000); */
-        });
       });
   };
->>>>>>> f9f9160c96d05acf545ae18714417ef66412d9d4
 
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <form onClick={this.handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
+        <div>
+          <form onClick={this.handleSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
 
-          <label htmlFor="">Password:</label>
-          <input
-            type="text"
-            id="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="email">email:</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-=======
-        <form action="">
-          <label htmlFor="">Name:</label>
-          <input
-            name="name"
-            id="name"
-            value={this.state.name}
-            onChange={this.setFormState}
-            type="text"
-          />
-          <label htmlFor="email">Email:</label>
-          <input
-            name="email"
-            id="email"
-            type="text"
-            value={this.state.email}
-            onChange={this.setFormState}
-          />
-          <label htmlFor="password">Password:</label>
-          <input
-            type="text"
-            name="password"
-            id="password"
-            value={this.state.password}
-            onChange={this.setFormState}
->>>>>>> akua
-          />
-          <label htmlFor="role">Role:</label>
-          <select
-            name="role"
-<<<<<<< HEAD
-            id="role"
-            value={this.state.role}
-            onChange={this.handleChange}
-          >
-            <option value="">Parent</option>
-            <option value="">Government</option>
-            <option value="">Owner</option>
-=======
-            value={this.state.role}
-            onChange={this.onChange}
-            id="role"
-          >
-            <option>parent</option>
-            <option>government</option>
-            <option>owner</option>
->>>>>>> akua
-          </select>
-          <button type="submit">Submit</button>
-        </form>
+            <label htmlFor="">Password:</label>
+            <input
+              type="text"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="email">email:</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </form>
+        </div>
+        <div>
+          <form action="">
+            <label htmlFor="">Name:</label>
+            <input
+              name="name"
+              id="name"
+              value={this.state.name}
+              onChange={this.setFormState}
+              type="text"
+            />
+            <label htmlFor="email">Email:</label>
+            <input
+              name="email"
+              id="email"
+              type="text"
+              value={this.state.email}
+              onChange={this.setFormState}
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              value={this.state.password}
+              onChange={this.setFormState}
+            />
+            <label htmlFor="role">Role:</label>
+            <select
+              name="role"
+              id="role"
+              value={this.state.role}
+              onChange={this.handleChange}
+            >
+              <option value="">Parent</option>
+              <option value="">Government</option>
+              <option value="">Owner</option>
+
+              <option
+                value={this.state.role}
+                onChange={this.onChange}
+                id="role"
+              />
+              <option>parent</option>
+              <option>government</option>
+              <option>owner</option>
+            </select>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
