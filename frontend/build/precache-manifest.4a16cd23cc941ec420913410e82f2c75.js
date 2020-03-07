9b81edd664e6df2ca85b578fbeafae6d406e7383
akups <appiah-lindberg@outlook.com>
2020-03-07 15:43:04 +0100

    deploy after homepage changes

---
+++ frontend/build/precache-manifest.4a16cd23cc941ec420913410e82f2c75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf50e6255181decc31d5e199f1e5703",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ebbb7d776e8dda117a8",
+    "url": "/static/css/2.3b80cb7c.chunk.css"
+  },
+  {
+    "revision": "3130006700d1a688ed9f",
+    "url": "/static/css/main.42813f67.chunk.css"
+  },
+  {
+    "revision": "8ebbb7d776e8dda117a8",
+    "url": "/static/js/2.af41c9cb.chunk.js"
+  },
+  {
+    "revision": "4886171caf23b7a1d2c3a0b6b10b94e4",
+    "url": "/static/js/2.af41c9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3130006700d1a688ed9f",
+    "url": "/static/js/main.9995027e.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  }
+]);
