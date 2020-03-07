9b015d8721ce8db2f1c82c40253ba14f13d5a4a6
akups <appiah-lindberg@outlook.com>
2020-03-07 13:28:22 +0100

    delopy

---
+++ frontend/build/precache-manifest.61e34a3d472345437997424d54a7b574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ce7ee14a84a3b824efc2435a5cc7d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35fbf9d662a6e5a89218",
+    "url": "/static/css/2.3b80cb7c.chunk.css"
+  },
+  {
+    "revision": "70970658a0ce21655b7d",
+    "url": "/static/css/main.1c1006d8.chunk.css"
+  },
+  {
+    "revision": "35fbf9d662a6e5a89218",
+    "url": "/static/js/2.1b7e92b2.chunk.js"
+  },
+  {
+    "revision": "4886171caf23b7a1d2c3a0b6b10b94e4",
+    "url": "/static/js/2.1b7e92b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70970658a0ce21655b7d",
+    "url": "/static/js/main.f566cfe8.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  }
+]);
