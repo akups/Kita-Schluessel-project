3673a05c8827afd7ac51635945fc7c8e6807031d
akups <appiah-lindberg@outlook.com>
2025-02-13 05:37:48 +0100

    handle deprecations

--- db/index.js
+++ db/index.js
@@ -1,21 +1,27 @@
 const mongoose = require("mongoose");
 
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
+//mongoose.set("useNewUrlParser", true);
+//mongoose.set("useFindAndModify", false);
+//mongoose.set("useCreateIndex", true);
+//mongoose.set("useUnifiedTopology", true);
 
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(conn => {
+  .connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true },
+    { useFindAndModify: false },
+    { useCreateIndex: true },
+    { useUnifiedTopology: true }
+  )
+  .then((conn) => {
     console.log(`Connected to ${conn.connections[0].name}`);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(`Error connecting to the DB: ${err}`);
   });
 
 module.exports = {
   disconnect: () => {
     mongoose.connection.close();
-  }
+  },
 };
